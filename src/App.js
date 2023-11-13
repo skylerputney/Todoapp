51a96d604380b3778346d321b556f6c602250690
skylerputney <skylerputney@gmail.com>
2023-11-13 11:43:37 -0500

    Lab 08

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App(){
+    const [todo, setTodo] = useState([]);
+    const todoInputRef = useRef(null);
 
-export default App;
+    const addTodo = () => {
+        const newTodo = todoInputRef.current.value;
+        if (newTodo !== '') {
+            setTodo([...todo, newTodo]);
+            todoInputRef.current.value = '';
+        }
+    };
+
+    const removeTodo = (index) => {
+        const updatedTodos = [...todo];
+        updatedTodos.splice(index, 1);
+        setTodo(updatedTodos);
+    };
+
+    return (
+        <div className="App">
+            <div className="container">
+                <h1>ToDo List</h1>
+                <div className="todo-input">
+                    <input type="text" ref={todoInputRef} placeholder="Enter a task to do" />
+                    <button onClick={addTodo}>Add</button>
+                </div>
+                <ul className="todo-list">
+                    {todo.map((todo, index) => (
+                        <li key={index} className={index % 2 === 0 ? 'even' : 'odd'}>
+                            {todo}
+                            <span className="text-dark" onClick={() => removeTodo(index)}>x</span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <footer>
+                <p>Background Image by </p>
+                <a href="https://www.freepik.com/free-photo/desktop-with-opened-organizer_2419952.htm#page=2&query=todo%20list%20horizontal&position=8&from_view=search&track=ais" rel="noopener noreferrer">Freepik</a>
+            </footer>
+
+        </div>
+    );
+};
+
